bd2cfad119f57e4b8f8346f74f6bf9fb223b8371
amriteshx1 <amriteshx18@gmail.com>
2025-04-28 16:27:07 +0530

    initial backend setup for the project

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const cors = require('cors');
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Home Route
+app.get('/', (req, res) => {
+    res.send('Welcome to Wordloom API!');
+});
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
