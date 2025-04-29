12143bc4f88744bb727832390969f03b4793edc9
amriteshx1 <amriteshx18@gmail.com>
2025-04-29 16:45:24 +0530

    Protect update/delete post routes with author-only access

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,18 +3,20 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 const commentController = require('../controllers/commentController');
 const { authenticateToken } = require('../middlewares/authMiddleware');
+const { authorizePostOwner } = require('../middlewares/authorize');
+const { authorizeCommentOwner } = require('../middlewares/authorize');
 
 router.get('/', authenticateToken, postController.getAllPosts);
 router.get('/:id', authenticateToken, postController.getPostbyId);
 router.post('/', authenticateToken, postController.createPost);
-router.put('/:id', authenticateToken, postController.updatePost);
-router.delete("/:id", authenticateToken, postController.deletePost);
+router.put('/:id', authenticateToken, authorizePostOwner, postController.updatePost);
+router.delete("/:id", authenticateToken, authorizePostOwner, postController.deletePost);
 router.patch('/:id/publish', authenticateToken, postController.publishPost);
 
 router.get('/:postId/comments', commentController.getAllComments);
 router.post('/:postId/comments', authenticateToken, commentController.addComment);
-router.put('/:postId/comments/:id', authenticateToken, commentController.editComment);
-router.delete("/:postId/comments/:id", authenticateToken, commentController.deleteComment);
+router.put('/:postId/comments/:id', authenticateToken, authorizeCommentOwner, commentController.editComment);
+router.delete("/:postId/comments/:id", authenticateToken, authorizeCommentOwner, commentController.deleteComment);
 
 
 module.exports = router;
