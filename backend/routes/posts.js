e49a9ce1aa1cdf2869fece0f82ee5e175e038def
amriteshx1 <amriteshx18@gmail.com>
2025-04-29 13:07:31 +0530

    feat: protect post routes using jwt authentication

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
-const commentController = require('../controllers/commentController')
+const commentController = require('../controllers/commentController');
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
-router.get('/', postController.getAllPosts);
-router.get('/:id', postController.getPostbyId);
-router.post('/', postController.createPost);
-router.put('/:id', postController.updatePost);
-router.delete("/:id", postController.deletePost);
-router.patch('/:id/publish', postController.publishPost);
+router.get('/', authenticateToken, postController.getAllPosts);
+router.get('/:id', authenticateToken, postController.getPostbyId);
+router.post('/', authenticateToken, postController.createPost);
+router.put('/:id', authenticateToken, postController.updatePost);
+router.delete("/:id", authenticateToken, postController.deletePost);
+router.patch('/:id/publish', authenticateToken, postController.publishPost);
 
 router.get('/:postId/comments', commentController.getAllComments);
-router.post('/:postId/comments', commentController.addComment);
-router.put('/:postId/comments/:id', commentController.editComment);
-router.delete("/:postId/comments/:id", commentController.deleteComment);
+router.post('/:postId/comments', authenticateToken, commentController.addComment);
+router.put('/:postId/comments/:id', authenticateToken, commentController.editComment);
+router.delete("/:postId/comments/:id", authenticateToken, commentController.deleteComment);
 
 
 module.exports = router;
