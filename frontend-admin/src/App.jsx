79cf7fa1e8c2e3ba55099d1dc801996203a96286
amriteshx1 <amriteshx18@gmail.com>
2025-05-02 19:55:21 +0530

    add child routes for proper navigation in dashboard

--- frontend-admin/src/App.jsx
+++ frontend-admin/src/App.jsx
@@ -1,15 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/loginpage';
 import Dashboard from "./pages/dashboard";
+import Home from "./pages/home";
+import Posts from "./pages/posts";
+import Comments from "./pages/comments";
+import Support from "./pages/support";
+
 
 function App() {
-  
 
   return (
     <Router>
       <Routes>
+
         <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+            <Route index element={<Home />} />
+            <Route path="posts" element={<Posts />} />
+            <Route path="comments" element={<Comments />} />
+            <Route path="support" element={<Support />} />
+        </Route>
+
       </Routes>
     </Router>
   );
