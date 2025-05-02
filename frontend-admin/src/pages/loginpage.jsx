55a5866aec2b669acf946ed87567814f6e928961
amriteshx1 <amriteshx18@gmail.com>
2025-05-02 07:52:27 +0530

    integrated Tailwind in the project and some minor tweaks

---
+++ frontend-admin/src/pages/loginpage.jsx
@@ -0,0 +1,11 @@
+function Login(){
+    return(
+        <div className="main-container">
+            <div className="inner-main-container">
+                <h1>Hey there!</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
