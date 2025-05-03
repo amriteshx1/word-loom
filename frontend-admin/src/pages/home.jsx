7184e8d69b694b5a3b65e8c41cce07fb9f3bf670
amriteshx1 <amriteshx18@gmail.com>
2025-05-03 07:49:27 +0530

    fetch userId from token and get real time user details

--- frontend-admin/src/pages/home.jsx
+++ frontend-admin/src/pages/home.jsx
@@ -1,8 +1,42 @@
+import { jwtDecode } from "jwt-decode";
+import { useState, useEffect } from "react";
 import searchimg from '../assets/search.png';
 import notificationimg from '../assets/notification.png';
 import profileimg from "../assets/profile.png";
 
 function Home(){
+    const [user, setUser] = useState(null);
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        const decodedToken = jwtDecode(token); 
+        const userId = decodedToken.id;
+        localStorage.setItem("userId", userId);
+      } else {
+        console.log("No token found!");
+      }
+
+      useEffect(() => {
+        const fetchUser = async () => {
+            try {
+              const userId = localStorage.getItem('userId');
+              const response = await fetch(`http://localhost:3000/api/users/${userId}`);
+          
+              if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+              }
+          
+              const data = await response.json();
+              setUser(data);
+            } catch (error) {
+              console.error("Error fetching user:", error);
+            }
+          };
+    
+        fetchUser();
+      }, []);
+
+
     return(
         <>
 
@@ -15,14 +49,14 @@ function Home(){
                         <div className='h-full w-[40%] flex justify-end items-center gap-[2vh]'>
                             <img src={notificationimg} alt="notification-logo" className='h-[70%] object-cover'/>
                             <img src={profileimg} alt="profile-logo" className='h-[80%] object-cover'/>
-                            <p className='text-[1.3vw] text-gray-700'>Anonymous</p>
+                            <p className='text-[1.3vw] text-gray-700'>{user?.username || "Username"}</p>
                         </div>
                     </div>
     
                     <div className='h-[60%] w-full flex justify-between items-center p-[2vh]'>
                         <div className='h-full w-[60%] flex flex-col justify-center items-start'>
                             <p className='text-[1.2vw] text-gray-700'>What's up,</p>
-                            <p className='text-[2.3vw] text-gray-700 font-medium'>Anonymous!</p>
+                            <p className='text-[2.3vw] text-gray-700 font-medium'>{user?.username || "User"}!</p>
                         </div>
     
                         <div className='h-full w-[40%] flex justify-end items-center'>
@@ -33,7 +67,8 @@ function Home(){
 
                 </div>
 
-                <div className='min-h-[70vh] w-full bg-gray-300'>
+                <div className='min-h-[70vh] w-full flex flex-col justify-start items-start gap-[2vh] p-[3vh] bg-gray-300'>
+                    <p>{user?.email || "abc@gmail.com"}</p>
 
                 </div>
                 
