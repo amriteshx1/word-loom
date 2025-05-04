72e31464c0ea78183bf0ee996a8fd9602a2da16b
amriteshx1 <amriteshx18@gmail.com>
2025-05-04 19:48:06 +0530

    add initial style to the blog's page of dashboard

--- frontend-admin/src/pages/posts.jsx
+++ frontend-admin/src/pages/posts.jsx
@@ -33,8 +33,31 @@ function Posts(){
 
     return(
         <>
-        <div className="min-h-screen w-full bg-gray-100">
-            <h1>See the console!</h1>
+        <div className="min-h-screen w-full flex flex-col justify-start items-center gap-[4vh] p-[2vh] bg-gray-100">
+            
+            <div className="w-[100%] flex flex-col justify-center items-center">
+            <p className="text-[2.3vw] text-gray-700 font-bold">Your Blogs!</p>
+            <hr className="w-[40%] border-2 border-gray-700" />
+            </div>
+
+
+            {posts && posts.length > 0 ? (
+                posts.map((item, index) => <div key={index} className="h-[7vh] w-full flex justify-between items-center p-[1vh] bg-gray-700 rounded-xl text-gray-200">
+                    <div className="h-full w-[50%] flex justify-between items-center">
+                        <p className="text-[1.2vw]"><span className="font-bold">Title: </span>{item.title}</p>
+                        <p className="text-[1.2vw]"><span className="font-bold">Id: </span>{item.id}</p>
+                    </div>
+                    
+                    <div className="h-full w-[25%] flex justify-between items-center">
+                        <button type="button">Edit</button>
+                        <button type="button">Publish/Unpublish</button>
+                    </div>
+                    
+                    
+                    </div>)
+            ) : (
+                <p>You don't have any posts.</p>
+            )}
         </div>
         </>
     )
