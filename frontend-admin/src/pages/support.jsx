f2933d7c49e126aa04637c42663c7e448c2658ff
amriteshx1 <amriteshx18@gmail.com>
2025-05-06 19:46:27 +0530

    update support page of the dashboard with content, style & layout

--- frontend-admin/src/pages/support.jsx
+++ frontend-admin/src/pages/support.jsx
@@ -1,9 +1,28 @@
 function Support(){
     return(
-        <div>
-            Hii, I am here to support!
+        <>
+        <div className="h-screen w-full flex flex-col justify-start items-center gap-[4vh] p-[2vh] bg-gray-100">
+            <div className="w-[100%] flex flex-col justify-center items-center">
+            <p className="text-[2.3vw] text-gray-700 font-bold">Get In Touch!</p>
+            <hr className="w-[30%] border-2 border-gray-700" />
+            </div>
+
+            <p className="text-[1.7vw] text-gray-700 font-semibold">Have a question, feedback, or need help with something? We're here to assist you.</p>
+
+            <div className="h-[20vh] w-full flex flex-col justify-center items-start p-[1vh] gap-[2vh] mt-[12vh] mb-[12vh]">
+                <p className="text-[1.4vw] text-gray-700 font-[550]">➤ Feel free to reach out for support regarding your account, posts, or any technical issues.</p> 
+                <p className="text-[1.2vw] text-gray-700 font-[550]">● You can contact us anytime at:</p>
+                <p className="text-[1.1vw] text-gray-700 font-[550]">- 📞 08831-3882-011</p>
+                <p className="text-[1.1vw] text-gray-700 font-[550]">- 📩 support@wordloom.com</p>
+                <p className="text-[1.2vw] text-gray-700 font-[550]">Our team typically responds within 24–48 hours.</p>
+            </div>
+
+            <p className="text-[1.4vw] text-gray-700 font-semibold">Thank you for being a part of our platform. Your feedback helps us grow and serve you better!🖤</p>
+            
+
         </div>
+        </>
     )
-}
+};
 
 export default Support;
