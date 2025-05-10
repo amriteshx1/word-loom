6ab379a7ded3142a79b1153332381791a963a136
amriteshx1 <amriteshx18@gmail.com>
2025-05-10 19:47:46 +0530

    initial router setup for the landing page

--- frontend-reader/src/App.jsx
+++ frontend-reader/src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage";
+
 function App(){
   return(
-    <div>
-      <h2 className="text-9xl">Hiiiiii!</h2>
-    </div>
+    <Router>
+      <Routes>
+
+        <Route path="/" element={<Homepage />} />
+
+      </Routes>
+    </Router>
   )
 }
 
