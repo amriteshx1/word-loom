49cf7dff278d88382778ec51a32d3bbe522bec3e
amriteshx1 <amriteshx18@gmail.com>
2025-05-12 19:35:37 +0530

    add initial navbar layout and style

--- frontend-reader/src/pages/homepage.jsx
+++ frontend-reader/src/pages/homepage.jsx
@@ -2,15 +2,34 @@ import homeimg from '../assets/homeimg.png';
 
 function Homepage(){
     return(
-        <div className="main-container p-[10vh]">
+        <div className="main-container flex-col pr-[10vh] pl-[10vh]">
 
-            <div className='h-[50vh] w-full flex flex-col justify-center items-start gap-[25px]'>
+            <div className='h-[10vh] w-full flex justify-between items-center bg-neutral-700 text-zinc-100 p-[2vh] rounded-b-4xl'>
+                <div className='w-[65%] flex justify-start items-center'>
+                    <p className='text-[2vw] font-medium'>Wordloom.</p>
+                </div>
+
+                <div className='w-[35%] flex justify-around items-center'>
+                    <p className='text-[1.1vw] font-medium'>Our Story</p>
+                    <p className='text-[1.1vw] font-medium'>Membership</p>
+                    <p className='text-[1.1vw] font-medium'>Sign in</p>
+                    <button className='text-[1vw] font-medium  p-[1vh] pr-[2vh] pl-[2vh] rounded-4xl bg-zinc-100 text-neutral-700 cursor-pointer hover:bg-zinc-200'>Get started</button>
+                </div>
+
+            </div>
+
+            <div className='h-[60vh] w-full flex justify-between items-center'>
+
+                <div className='h-full w-full flex flex-col justify-center items-start gap-[25px]'>
                 <p className='text-[6vw] font-bold bg-gradient-to-tl from-neutral-950 via-zinc-500 to-neutral-700 bg-clip-text text-transparent'>Weave thoughts <br /> into words</p>
                 <p className='text-[1.8vw] font-medium bg-gradient-to-tl from-neutral-950 via-zinc-500 to-neutral-700 bg-clip-text text-transparent'>A space to write, reflect, and connect with meaning!</p>
                 <button className='text-[1.4vw] font-medium bg-neutral-700 w-[13vw] p-[1vh] rounded-4xl text-zinc-100 cursor-pointer hover:bg-neutral-800'>Start reading</button>
-            </div>
+                </div>
 
-            <img src={homeimg} alt="home img" className='h-[60vh] object-cover' />
+                <img src={homeimg} alt="home img" className='h-[60vh] object-cover' />
+
+            </div>
+            
         </div>
     )
 }
