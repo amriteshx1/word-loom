995c91192326f01e9bf7f887783dbd8ff4b3ea55
amriteshx1 <amriteshx18@gmail.com>
2025-05-16 10:58:39 +0530

    some minor tweaks

--- frontend-reader/src/pages/signup.jsx
+++ frontend-reader/src/pages/signup.jsx
@@ -18,30 +18,30 @@ export default function Signup({isOpen, onClose}){
                 <button onClick={onClose} className=" absolute top-0 right-0 p-[1vh] pr-[2.5vh] cursor-pointer text-[1.5vw] text-gray-500 hover:text-gray-700">
                   x
                 </button>
-                <h2 className="text-[1.8vw] font-medium mb-[6vh]">Join Wordloom.</h2>
+                <h2 className="text-[1.9vw] text-neutral-900 font-medium mb-[6vh]">Join Wordloom.</h2>
 
-                <form className="flex flex-col justify-center items-center w-[60%] gap-3">
-                    <div className="flex flex-col justify-center items-start gap-1 w-full">
+                <form className="flex flex-col justify-center items-center w-[60%] gap-[1.5vh]">
+                    <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
                         <label htmlFor="username" className="text-[1.1vw] text-neutral-900">Username</label>
-                        <input type="text" id="username" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl" />
+                        <input type="text" id="username" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]" />
                     </div>
 
-                    <div className="flex flex-col justify-center items-start gap-1 w-full">
+                    <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
                         <label htmlFor="email" className="text-[1.1vw] text-neutral-900">Email</label>
-                        <input type="email" id="email" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl"/>
+                        <input type="email" id="email" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
                     </div>
 
-                    <div className="flex flex-col justify-center items-start gap-1 w-full">
+                    <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
                         <label htmlFor="password" className="text-[1.1vw] text-neutral-900">Password</label>
-                        <input type="password" id="password" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl"/>
+                        <input type="password" id="password" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
                     </div>
 
-                    <div className="flex flex-col justify-center items-start gap-1 w-full">
+                    <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
                         <label htmlFor="confirmPassword" className="text-[1.1vw] text-neutral-900">Confirm Password</label>
-                        <input type="password" id="confirmPassword" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl"/>
+                        <input type="password" id="confirmPassword" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
                     </div>
             
-                  <button className="bg-neutral-700 w-[50%] text-white p-[1vh] pr-[2vh] pl-[2vh] rounded-xl hover:bg-neutral-800 mt-[2vh] cursor-pointer">
+                  <button className="bg-neutral-700 w-[50%] text-white p-[1vh] pr-[2vh] pl-[2vh] rounded-xl hover:bg-neutral-800 mt-[1vh] cursor-pointer">
                     Sign up
                   </button>
                 </form>
