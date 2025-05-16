c39dc6516c2cee22ef71e345be630ea9d8823a4e
amriteshx1 <amriteshx18@gmail.com>
2025-05-16 18:27:23 +0530

    add handleLogin logic for loggin-in user to the website

--- frontend-reader/src/pages/signin.jsx
+++ frontend-reader/src/pages/signin.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Signin({isOpen, onClose, onSwitchToSignUp}){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
     if(!isOpen) return null;
 
     const handleOverlayClick = (e) => {
@@ -8,6 +13,30 @@ export default function Signin({isOpen, onClose, onSwitchToSignUp}){
       onClose();
     }
    };
+
+   const handleLogin = async (e) => {
+        e.preventDefault();
+    
+        try {
+          const res = await fetch("http://localhost:3000/api/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password }),
+          });
+    
+          const data = await res.json();
+    
+          if (res.ok) {
+            localStorage.setItem("token", data.token);
+            navigate("/feed");
+          } else {
+            alert(data.message || "Login failed");
+          }
+        } catch (err) {
+          alert("Server error");
+          console.error(err);
+        }
+      };
 
     return (
         <div 
@@ -20,16 +49,16 @@ export default function Signin({isOpen, onClose, onSwitchToSignUp}){
                 </button>
                 <h2 className="text-[1.9vw] text-neutral-900 font-medium mb-[6vh]">Welcome back.</h2>
 
-                <form className="flex flex-col justify-center items-center w-[60%] gap-[1.5vh]">
+                <form className="flex flex-col justify-center items-center w-[60%] gap-[1.5vh]" onSubmit={handleLogin}>
 
                     <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
                         <label htmlFor="email" className="text-[1.1vw] text-neutral-900">Email</label>
-                        <input type="email" id="email" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
+                        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
                     </div>
 
                     <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
                         <label htmlFor="password" className="text-[1.1vw] text-neutral-900">Password</label>
-                        <input type="password" id="password" required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
+                        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
                     </div>
 
                     <p className="text-[1vw] text-neutral-900">No account? <span
@@ -38,7 +67,7 @@ export default function Signin({isOpen, onClose, onSwitchToSignUp}){
                       onSwitchToSignUp();
                     }} className="text-[1vw] text-neutral-950 font-medium cursor-pointer hover:underline">Create one</span></p>
 
-                  <button className="bg-neutral-700 w-[50%] text-white p-[1vh] pr-[2vh] pl-[2vh] rounded-xl hover:bg-neutral-800 mt-[1vh] cursor-pointer">
+                  <button type="submit" className="bg-neutral-700 w-[50%] text-white p-[1vh] pr-[2vh] pl-[2vh] rounded-xl hover:bg-neutral-800 mt-[1vh] cursor-pointer">
                     Sign in
                   </button>
                 </form>
@@ -47,4 +76,4 @@ export default function Signin({isOpen, onClose, onSwitchToSignUp}){
             
         </div>
     )
-}
+};
