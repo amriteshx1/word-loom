7bbc01aeebb34b70bb432f5d32605b56c1596adf
amriteshx1 <amriteshx18@gmail.com>
2025-05-16 19:42:30 +0530

    add logic for fetching all blogs from the backend

--- frontend-reader/src/pages/feed.jsx
+++ frontend-reader/src/pages/feed.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function Feed(){
+    const [posts, setPosts] = useState([])
     const navigate = useNavigate();
 
     const handleLogout = () => {
@@ -11,6 +13,27 @@ export default function Feed(){
           navigate("/");
         }
     };
+
+    useEffect(() => {
+        const fetchPosts = async() => {
+            const token = localStorage.getItem("token");
+
+            const res = await fetch(`http://localhost:3000/api/posts`, {
+              method: "GET",
+              headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}`, },
+            });
+
+            if (!res.ok) throw new Error("Failed to fetch posts");
+
+            const data = await res.json();
+            console.log(data);
+            setPosts(data);
+        }
+
+        fetchPosts();
+    }, []);
+    
+
     return(
         <div>
         <h1>Hiii u are at feed!</h1>
