054c1678588376ceded041697813dc04e32819b1
amriteshx1 <amriteshx18@gmail.com>
2025-05-16 18:28:29 +0530

    add basic logout functionality in the homefeed

--- frontend-reader/src/pages/feed.jsx
+++ frontend-reader/src/pages/feed.jsx
@@ -1,7 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function Feed(){
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        const confirmLogout = window.confirm("Are you sure you want to log out?");
+        if (confirmLogout) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("userId");
+          navigate("/");
+        }
+    };
     return(
         <div>
         <h1>Hiii u are at feed!</h1>
+        <div onClick={handleLogout}>
+            <p>Sign Out</p>
+        </div>
         </div>
     )
 }
