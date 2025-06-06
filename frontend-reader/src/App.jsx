aab74f16af68c93134a5a1240e9a669bd3873308
amriteshx1 <amriteshx18@gmail.com>
2025-06-06 13:57:44 +0530

    setup main app routes with homepage, feed layout, and post view

--- frontend-reader/src/App.jsx
+++ frontend-reader/src/App.jsx
@@ -2,19 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import Feed from "./pages/feed";
 import Post from "./pages/post";
+import Layout from "./pages/layout"; 
 
 function App(){
   return(
     <Router>
       <Routes>
-
         <Route path="/" element={<Homepage />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/feed/post/:id" element={<Post />} />
-
+        <Route path="/feed" element={<Layout />}>
+          <Route index element={<Feed />} />
+          <Route path="post/:id" element={<Post />} />
+        </Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
