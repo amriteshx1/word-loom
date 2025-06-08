4e9908915d545744ae64a57bbfc6e88d6a35151c
amriteshx1 <amriteshx18@gmail.com>
2025-06-08 16:25:11 +0530

    restlyed support-page to match admin vibes

--- frontend-admin/src/pages/support.jsx
+++ frontend-admin/src/pages/support.jsx
@@ -1,23 +1,28 @@
+import phone from "../assets/call.png";
+import mail from "../assets/mail.png";
+
 function Support(){
     return(
         <>
-        <div className="h-screen w-full flex flex-col justify-start items-center gap-[4vh] p-[2vh] bg-gray-100">
-            <div className="w-[100%] flex flex-col justify-center items-center">
-            <p className="text-[2.3vw] text-gray-700 font-bold">Get In Touch!</p>
-            <hr className="w-[30%] border-2 border-gray-700" />
+        <div className="h-screen w-[90%] flex flex-col justify-start items-center gap-[4vh] p-[2vh] bg-neutral-800">
+            <div className="w-[100%] flex flex-col justify-center items-center p-[2vh]">
+            <p className="text-[2.3vw] text-white font-bold">Get In Touch!</p>
+            <hr className="w-[30%] border-2 border-white" />
             </div>
 
-            <p className="text-[1.7vw] text-gray-700 font-semibold">Have a question, feedback, or need help with something? We're here to assist you.</p>
+            <p className="text-[1.7vw] text-white font-medium">Have a question, feedback, or need help with something? We're here to assist you.🤝</p>
 
-            <div className="h-[20vh] w-full flex flex-col justify-center items-start p-[1vh] gap-[2vh] mt-[12vh] mb-[12vh]">
-                <p className="text-[1.4vw] text-gray-700 font-[550]">➤ Feel free to reach out for support regarding your account, posts, or any technical issues.</p> 
-                <p className="text-[1.2vw] text-gray-700 font-[550]">● You can contact us anytime at:</p>
-                <p className="text-[1.1vw] text-gray-700 font-[550]">- 📞 08831-3882-011</p>
-                <p className="text-[1.1vw] text-gray-700 font-[550]">- 📩 support@wordloom.com</p>
-                <p className="text-[1.2vw] text-gray-700 font-[550]">Our team typically responds within 24–48 hours.</p>
+            <div className="w-full flex flex-col justify-center items-start rounded-2xl p-[7vh] gap-[2vh] mt-[6vh] mb-[6vh] bg-neutral-900">
+                <p className="text-[1.4vw] text-white font-medium">✨ Feel free to reach out for support regarding your account, posts, or any technical issues.</p>
+                <div className="w-full flex flex-col justify-center items-start gap-[2vh] p-[3vh]">
+                <p className="text-[1.2vw] text-white">You can contact us anytime at:</p>
+                <p className="flex justify-center items-center gap-[2vh] text-[1.1vw] text-white"><img src={phone} alt="call-logo" className="h-[2.7vh] object-cover"/>  08831-3882-011</p>
+                <p className="flex justify-center items-center gap-[2vh] text-[1.1vw] text-white"><img src={mail} alt="email-logo" className="h-[2.7vh] object-cover" />  support@wordloom.com</p>
+                <p className="text-[1.2vw] text-white">Our team typically responds within 24–48 hours.</p>
+                </div>
             </div>
 
-            <p className="text-[1.4vw] text-gray-700 font-semibold">Thank you for being a part of our platform. Your feedback helps us grow and serve you better!🖤</p>
+            <p className="text-[1.4vw] text-white font-medium ">Thank you for being a part of our platform. Your feedback helps us grow and serve you better! 🤍</p>
             
 
         </div>
