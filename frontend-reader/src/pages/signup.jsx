f4f8cb8c28c85c173f2f7493c5d9494f08c941ed
amriteshx1 <amriteshx18@gmail.com>
2025-06-10 09:03:53 +0530

    add responsiveness to the sign-up model

--- frontend-reader/src/pages/signup.jsx
+++ frontend-reader/src/pages/signup.jsx
@@ -44,52 +44,52 @@ export default function Signup({isOpen, onClose, heading = "Join Wordloom.", onS
         className="fixed inset-0 bg-white/93 flex justify-center items-center z-50"
         onClick={handleOverlayClick}
         >
-            <div className="bg-white p-6 rounded-xl h-[90%] w-[90%] max-h-[85vh] max-w-[40vw] shadow-lg flex flex-col justify-center items-center relative motion-preset-slide-up motion-duration-500">
-                <button onClick={onClose} className=" absolute top-0 right-0 p-[1vh] pr-[2.5vh] cursor-pointer text-[1.5vw] text-gray-500 hover:text-gray-700">
+            <div className="bg-white p-6 rounded-xl h-[90%] w-[90%] lg:max-h-[85vh] lg:max-w-[40vw] max-h-[70vh] sm:max-w-[70vw] max-w-[85vw] shadow-lg flex flex-col justify-center items-center relative motion-preset-slide-up motion-duration-500">
+                <button onClick={onClose} className=" absolute top-0 right-0 p-[1vh] pr-[2.5vh] cursor-pointer lg:text-[1.5vw] text-[1.8vh] text-gray-500 hover:text-gray-700">
                   x
                 </button>
-                <h2 className="text-[1.8vw] text-neutral-900 font-medium mb-[6vh]">{heading}</h2>
+                <h2 className="lg:text-[1.8vw] sm:text-[2.2vh] text-[2vh] text-neutral-900 font-medium mb-[6vh]">{heading}</h2>
 
-                <form className="flex flex-col justify-center items-center w-[60%] gap-[1.5vh]" onSubmit={handleSubmit}>
+                <form className="flex flex-col justify-center items-center lg:w-[60%] w-[80%] gap-[1.5vh]" onSubmit={handleSubmit}>
                     <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
-                        <label htmlFor="username" className="text-[1.1vw] text-neutral-900">Username</label>
+                        <label htmlFor="username" className="lg:text-[1.1vw] text-[1.4vh] text-neutral-900">Username</label>
                         <input type="text" id="username" required 
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
-                        className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]" />
+                        className="lg:h-[5vh] h-[3.5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh] focus:outline-none" />
                     </div>
 
                     <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
-                        <label htmlFor="email" className="text-[1.1vw] text-neutral-900">Email</label>
+                        <label htmlFor="email" className="lg:text-[1.1vw] text-[1.4vh] text-neutral-900">Email</label>
                         <input type="email" id="email" required 
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
+                        className="lg:h-[5vh] h-[3.5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh] focus:outline-none" />
                     </div>
 
                     <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
-                        <label htmlFor="password" className="text-[1.1vw] text-neutral-900">Password</label>
+                        <label htmlFor="password" className="lg:text-[1.1vw] text-[1.4vh] text-neutral-900">Password</label>
                         <input type="password" id="password" required 
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
-                        className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
+                        className="lg:h-[5vh] h-[3.5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh] focus:outline-none" />
                     </div>
 
                     <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
-                        <label htmlFor="confirmPassword" className="text-[1.1vw] text-neutral-900">Confirm Password</label>
+                        <label htmlFor="confirmPassword" className="lg:text-[1.1vw] text-[1.4vh] text-neutral-900">Confirm Password</label>
                         <input type="password" id="confirmPassword" required 
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
-                        className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
+                        className="lg:h-[5vh] h-[3.5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh] focus:outline-none" />
                     </div>
 
-                    <p className="text-[1vw] text-neutral-900">Already have an account? <span 
+                    <p className="lg:text-[1vw] text-[1.1vh] text-neutral-900">Already have an account? <span 
                     onClick={() => {
                          onClose(); 
                          onSwitchToSignin();
-                        }} className="text-[1vw] text-neutral-950 font-medium cursor-pointer hover:underline">Sign in</span></p>
+                        }} className="lg:text-[1vw] text-[1.1vh] text-neutral-950 font-medium cursor-pointer hover:underline">Sign in</span></p>
 
-                  <button type="submit" className="bg-neutral-700 w-[50%] text-white p-[1vh] pr-[2vh] pl-[2vh] rounded-xl hover:bg-neutral-800 mt-[1vh] cursor-pointer">
+                  <button type="submit" className="bg-neutral-700 lg:text-[1.1vw] text-[1.4vh] lg:w-[50%] sm:w-[35%] w-[50%] text-white p-[1vh] pr-[2vh] pl-[2vh] rounded-xl hover:bg-neutral-800 mt-[1vh] cursor-pointer">
                     Sign up
                   </button>
                 </form>
