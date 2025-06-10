bbc0e662fb272ee992ab6fad5b1659be4954d42b
amriteshx1 <amriteshx18@gmail.com>
2025-06-10 09:49:20 +0530

    add responsiveness to the sign-in model

--- frontend-reader/src/pages/signin.jsx
+++ frontend-reader/src/pages/signin.jsx
@@ -43,31 +43,31 @@ export default function Signin({isOpen, onClose, onSwitchToSignUp}){
         className="fixed inset-0 bg-white/93 flex justify-center items-center z-50"
         onClick={handleOverlayClick}
         >
-             <div className="bg-white p-6 rounded-xl h-[90%] w-[90%] max-h-[85vh] max-w-[40vw] shadow-lg flex flex-col justify-center items-center relative motion-preset-slide-up motion-duration-500">
-                <button onClick={onClose} className=" absolute top-0 right-0 p-[1vh] pr-[2.5vh] cursor-pointer text-[1.5vw] text-gray-500 hover:text-gray-700">
+             <div className="bg-white p-6 rounded-xl h-[90%] w-[90%] lg:max-h-[85vh] lg:max-w-[40vw] max-h-[70vh] sm:max-w-[70vw] max-w-[85vw] shadow-lg flex flex-col justify-center items-center relative motion-preset-slide-up motion-duration-500">
+                <button onClick={onClose} className=" absolute top-0 right-0 p-[1vh] pr-[2.5vh] cursor-pointer lg:text-[1.5vw] text-[1.8vh] text-gray-500 hover:text-gray-700">
                   x
                 </button>
-                <h2 className="text-[1.9vw] text-neutral-900 font-medium mb-[6vh]">Welcome back.</h2>
+                <h2 className="lg:text-[1.8vw] sm:text-[2.2vh] text-[2vh] text-neutral-900 font-medium mb-[6vh]">Welcome back.</h2>
 
-                <form className="flex flex-col justify-center items-center w-[60%] gap-[1.5vh]" onSubmit={handleLogin}>
+                <form className="flex flex-col justify-center items-center lg:w-[60%] w-[80%] gap-[1.5vh]" onSubmit={handleLogin}>
 
                     <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
-                        <label htmlFor="email" className="text-[1.1vw] text-neutral-900">Email</label>
-                        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
+                        <label htmlFor="email" className="lg:text-[1.1vw] text-[1.4vh] text-neutral-900">Email</label>
+                        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required className="lg:h-[5vh] h-[3.5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh] focus:outline-none"/>
                     </div>
 
                     <div className="flex flex-col justify-center items-start gap-[0.2vh] w-full">
-                        <label htmlFor="password" className="text-[1.1vw] text-neutral-900">Password</label>
-                        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required className="h-[5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh]"/>
+                        <label htmlFor="password" className="lg:text-[1.1vw] text-[1.4vh] text-neutral-900">Password</label>
+                        <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required className="lg:h-[5vh] h-[3.5vh] w-full border-2 border-neutral-700 rounded-xl p-[1vh] focus:outline-none"/>
                     </div>
 
-                    <p className="text-[1vw] text-neutral-900">No account? <span
+                    <p className="lg:text-[1vw] text-[1.1vh] text-neutral-900">No account? <span
                     onClick={() => {
                       onClose();
                       onSwitchToSignUp();
-                    }} className="text-[1vw] text-neutral-950 font-medium cursor-pointer hover:underline">Create one</span></p>
+                    }} className="lg:text-[1vw] text-[1.1vh] text-neutral-950 font-medium cursor-pointer hover:underline">Create one</span></p>
 
-                  <button type="submit" className="bg-neutral-700 w-[50%] text-white p-[1vh] pr-[2vh] pl-[2vh] rounded-xl hover:bg-neutral-800 mt-[1vh] cursor-pointer">
+                  <button type="submit" className="bg-neutral-700 lg:text-[1.1vw] text-[1.4vh] lg:w-[50%] sm:w-[35%] text-white p-[1vh] pr-[2vh] pl-[2vh] rounded-xl hover:bg-neutral-800 mt-[1vh] cursor-pointer">
                     Sign in
                   </button>
                 </form>
