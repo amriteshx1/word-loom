f7f75b323fe41322422649ac4a5415cd95333e18
amriteshx1 <amriteshx18@gmail.com>
2025-06-10 13:48:55 +0530

    add responsiveness to the main-feed

--- frontend-reader/src/pages/feed.jsx
+++ frontend-reader/src/pages/feed.jsx
@@ -119,17 +119,17 @@ export default function Feed(){
 
     return(
 
-            <div className='h-[91vh] w-full flex justify-between items-center pt-[5vh]'>
+            <div className='h-[91vh] w-full flex justify-between items-center lg:pt-[5vh] pt-[2vh]'>
 
-              <div className='h-full w-[60%] flex flex-col justify-start items-center overflow-y-scroll'>
+              <div className='h-full lg:w-[60%] w-full flex flex-col justify-start items-center overflow-y-scroll'>
 
                 <div className='h-[12%] w-full flex flex-col justify-start gap-1 items-start px-4'>
-                  <p className='text-[2.3vw] font-semibold bg-gradient-to-tl from-neutral-950 via-zinc-500 to-neutral-700 bg-clip-text text-transparent'>❝ {greeting || "Welcome back!"}</p>
+                  <p className='lg:text-[2.3vw] text-[2.3vh] font-semibold bg-gradient-to-tl from-neutral-950 via-zinc-500 to-neutral-700 bg-clip-text text-transparent'>❝ {greeting || "Welcome back!"}</p>
                   <hr className='w-full border-neutral-200' />
                 </div>
 
-                <div className='h-[10%] w-full flex flex-col justify-end items-start mt-[7vh] gap-1 px-4'>
-                  <p className='text-xl font-semibold text-neutral-700'>For You :</p>
+                <div className='h-[10%] w-full flex flex-col justify-end items-start lg:mt-[7vh] mt-[3vh] gap-1 px-4'>
+                  <p className='lg:text-xl sm:text-lg text-base font-semibold text-neutral-700'>For You :</p>
                   <hr className='w-[10%] border-neutral-200' />
                 </div>
 
@@ -137,8 +137,8 @@ export default function Feed(){
                 {posts.map(post => (
                 <div key={post.id} className="h-[30vh] w-full flex justify-between items-center p-4 rounded-xl bg-white shadow-md">
                   <div className='h-full w-[70%] flex flex-col justify-around items-start'>
-                  <p onClick={() => handlePost(post.id)} className="text-2xl font-semibold mb-1 text-neutral-700 hover:underline cursor-pointer">{post.title}</p>
-                  <div className="text-[1vw] text-neutral-600 mb-2">
+                  <p onClick={() => handlePost(post.id)} className="lg:text-2xl sm:text-xl text-lg font-semibold mb-1 text-neutral-700 hover:underline cursor-pointer">{post.title}</p>
+                  <div className="lg:text-[1vw] text-[1.2vh] text-neutral-600 mb-2">
                     By <span className='font-medium'>{post.author.username} </span> | {new Date(post.createdAt).toLocaleDateString('en-US', {
                     year: 'numeric',
                     month: 'long',
@@ -146,28 +146,28 @@ export default function Feed(){
                   })}
                   </div>
                   <div
-                    className="prose max-w-none font-medium text-neutral-700 line-clamp-2"
+                    className="prose-sm sm:prose max-w-none font-medium text-neutral-700 line-clamp-2"
                     dangerouslySetInnerHTML={{ __html: post.content }}
                   />
 
-                  <div className='flex justify-start items-center mt-[1vh] gap-[2vw]'>
+                  <div className='flex justify-start items-center mt-[1vh] lg:gap-[2vw] gap-[2vh]'>
                     <div onClick={() => increaseLike(post.id)} className= {`flex items-center gap-[0.2vw] cursor-pointer transition-transform duration-150 ${
                       clickedId === post.id ? 'scale-90' : ''}`}>
 
-                      <HeartHandshake style={{height: '2.5vh'}} />
-                      <span className="text-neutral-700 font-medium">{post.likes}</span>
+                      <HeartHandshake className="lg:h-[2.5vh] sm:h-[1.9vh] h-[1.7vh]" />
+                      <span className="text-neutral-700 lg:text-[1vw] text-[1.2vh] font-medium">{post.likes}</span>
                     </div>
                     
-                    <div className='flex items-center gap-[0.3vw] cursor-pointer'>
-                      <img src={comments} alt="comment-logo" className='h-[2.5vh] object-cover' />
-                      <span className="text-neutral-700 font-medium">{post._count.comments}</span>
+                    <div className='flex items-center lg:gap-[0.3vw] sm:gap-[0.2vh] gap-[0.5vh] cursor-pointer'>
+                      <img src={comments} alt="comment-logo" className='lg:h-[2.5vh] sm:h-[1.9vh] h-[1.7vh] object-cover' />
+                      <span className="text-neutral-700 lg:text-[1vw] text-[1.2vh] font-medium">{post._count.comments}</span>
                     </div>
                   </div>
                   
                 </div>
 
                 <div className='h-full w-[25%] flex justify-center items-center'>
-                  <img src={post.thumbnail} alt="blog-thumbnail" className='h-[80%] object-cover rounded-xl'/>
+                  <img src={post.thumbnail} alt="blog-thumbnail" className='lg:h-[80%] sm:h-[65%] h-[55%] object-cover rounded-xl'/>
                 </div>
 
                 </div>
@@ -175,7 +175,7 @@ export default function Feed(){
               </div>
               </div>
 
-              <div className='h-full w-[30%] flex flex-col justify-between items-center pb-[5vh]'>
+              <div className='hidden lg:flex h-full w-[30%] flex-col justify-between items-center pb-[5vh]'>
                 <div className='h-[45%] w-full flex flex-col justify-between items-start bg-neutral-700 rounded-2xl p-[2vh]'>
                   <div className='flex'>
                     <p className='text-[1.2vw] text-white font-medium'>Trending</p>
