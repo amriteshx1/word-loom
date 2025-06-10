c18e9e86a2e7d27a63d15c17dd2934f3daf245f1
amriteshx1 <amriteshx18@gmail.com>
2025-06-10 15:06:58 +0530

    add responsiveness to the blog main-page

--- frontend-reader/src/pages/post.jsx
+++ frontend-reader/src/pages/post.jsx
@@ -93,13 +93,13 @@ export default function Post(){
 
 
     return(
-        <div className="h-[91vh] w-[50%] flex flex-col justify-start items-center gap-[5vh]">
+        <div className="h-[91vh] lg:w-[50%] sm:w-[70%] w-[80%] flex flex-col justify-start items-center lg:gap-[5vh] gap-[4vh]">
         
             {post ? (
               <>
-              <div className="max-h-[50%] w-full flex flex-col justify-start items-start mt-[10vh] gap-[3vh]">
-                <p className="text-[3vw] text-neutral-700 font-bold">{post.title}</p>
-                <p className="text-[1.2vw] text-neutral-700 font-medium">By <span className="text-[1.3vw] text-neutral-700 font-semibold">{post.author.username} </span>
+              <div className="max-h-[50%] w-full flex flex-col justify-start items-start lg:mt-[10vh] mt-[5vh] gap-[3vh]">
+                <p className="lg:text-[3vw] text-[3vh] text-neutral-700 font-bold">{post.title}</p>
+                <p className="lg:text-[1.2vw] sm:text-[1.2vh] text-[1.5vh] text-neutral-700 font-medium">By <span className="lg:text-[1.3vw] sm:text-[1.3vh] text-[1.6vh] text-neutral-700 font-semibold">{post.author.username} </span>
                 • {new Date(post.createdAt).toLocaleDateString('en-US', {
                     year: 'numeric',
                     month: 'long',
@@ -111,13 +111,13 @@ export default function Post(){
                 <div className="w-full flex justify-start items-center gap-[3vw]">
                   <div onClick={() => increaseLike(post.id)} className= {`flex items-center gap-[0.2vw] cursor-pointer transition-transform duration-150 ${
                       clickedId === post.id ? 'scale-90' : ''}`}>
-                    <HeartHandshake style={{height: '2.5vh'}} />
-                    <span className="text-neutral-700 font-medium">{post.likes}</span>
+                    <HeartHandshake className="lg:h-[2.5vh] sm:h-[1.9vh] h-[1.7vh]" />
+                    <span className="text-neutral-700 lg:text-[1vw] sm:text-[1.2vh] text-[1.4vh] font-medium">{post.likes}</span>
                   </div>
 
-                  <div className='flex items-center gap-[0.3vw] cursor-pointer'>
-                    <img src={commentLogo} alt="comment-logo" className='h-[2.5vh] object-cover' />
-                    <span className="text-neutral-700 font-medium">{post._count.comments}</span>
+                  <div className='flex items-center lg:gap-[0.3vw] sm:gap-[0.2vh] gap-[0.5vh] cursor-pointer'>
+                    <img src={commentLogo} alt="comment-logo" className='lg:h-[2.5vh] sm:h-[1.9vh] h-[1.7vh] object-cover' />
+                    <span className="text-neutral-700 lg:text-[1vw] sm:text-[1.2vh] text-[1.4vh] font-medium">{post._count.comments}</span>
                   </div>
                 </div>
                 <hr className="w-full border-neutral-200 " />
@@ -137,37 +137,37 @@ export default function Post(){
                 <div className="w-full flex justify-start items-center gap-[3vw]">
                   <div onClick={() => increaseLike(post.id)} className= {`flex items-center gap-[0.2vw] cursor-pointer transition-transform duration-150 ${
                     clickedId === post.id ? 'scale-90' : ''}`}>
-                    <HeartHandshake style={{height: '2.5vh'}} />
-                    <span className="text-neutral-700 font-medium">{post.likes}</span>
+                    <HeartHandshake className="lg:h-[2.5vh] sm:h-[1.9vh] h-[1.7vh]" />
+                    <span className="text-neutral-700 lg:text-[1vw] sm:text-[1.2vh] text-[1.4vh] font-medium">{post.likes}</span>
                   </div>
 
-                  <div className='flex items-center gap-[0.3vw] cursor-pointer'>
-                    <img src={commentLogo} alt="comment-logo" className='h-[2.5vh] object-cover' />
-                    <span className="text-neutral-700 font-medium">{post._count.comments}</span>
+                  <div className='flex items-center lg:gap-[0.3vw] sm:gap-[0.2vh] gap-[0.5vh] cursor-pointer'>
+                    <img src={commentLogo} alt="comment-logo" className='lg:h-[2.5vh] sm:h-[1.9vh] h-[1.7vh] object-cover' />
+                    <span className="text-neutral-700 lg:text-[1vw] sm:text-[1.2vh] text-[1.4vh] font-medium">{post._count.comments}</span>
                   </div>
                 </div>
                 <hr className="w-full border-neutral-200 " />
                 </div>
 
-                <p className="text-[1.5vw] text-neutral-700 font-medium self-start">Responses ({post._count.comments})</p>
+                <p className="lg:text-[1.5vw] sm:text-[1.6vh] text-[1.7vh] text-neutral-700 font-medium self-start">Responses ({post._count.comments})</p>
 
                 <div className="w-full flex justify-between items-center">
-                  <textarea className="w-[80%] border border-neutral-300 rounded-2xl p-[1vh]" rows={2} placeholder="What are your thoughts?" value={newComment} onChange={(e) => setNewComment(e.target.value)}></textarea>
-                  <button onClick={handleAddComment} className="px-[2vh] py-[1vh] bg-neutral-700 text-white rounded-2xl hover:bg-neutral-800 cursor-pointer">Respond</button>
+                  <textarea className="lg:w-[80%] w-[65%] lg:text-[1.1vw] text-[1.1vh] border border-neutral-300 rounded-2xl p-[1vh]" rows={2} placeholder="What are your thoughts?" value={newComment} onChange={(e) => setNewComment(e.target.value)}></textarea>
+                  <button onClick={handleAddComment} className="px-[2vh] py-[1vh] lg:text-[1vw] text-[1.1vh] bg-neutral-700 text-white rounded-2xl hover:bg-neutral-800 cursor-pointer">Respond</button>
                 </div>
                 
-                <div className="flex flex-col w-full justify-start items-center gap-[5vh]">
+                <div className="flex flex-col w-full justify-start items-center lg:gap-[5vh] gap-[4vh]">
                   {comments.map((comment) => (
                     <div key={comment.id} className="flex flex-col justify-start items-start w-full gap-[1vh]">
                       <div className="flex justify-start items-center gap-[1vh]">
-                        <img src={commentUser} alt="user-comment logo" className="h-[5.2vh]" />
+                        <img src={commentUser} alt="user-comment logo" className="lg:h-[5.2vh] sm:h-[3vh] h-[3.3vh]" />
                         <div className="flex flex-col justify-start items-start">
-                          <p className="text-[1vw] text-neutral-900 font-medium">{comment.author.username}</p>
-                          <p className="text-[0.8vw] text-neutral-500">{formatDistanceToNow(new Date(comment.createdAt), { addSuffix: true })}</p>
+                          <p className="lg:text-[1vw] sm:text-[1vh] text-[1.1vh] text-neutral-900 font-medium">{comment.author.username}</p>
+                          <p className="lg:text-[0.8vw] sm:text-[0.9vh] text-[1vh] text-neutral-500">{formatDistanceToNow(new Date(comment.createdAt), { addSuffix: true })}</p>
                         </div>
                       </div>
                   
-                      <p className="text-[1vw] text-neutral-700 font-medium">{comment.content}</p>
+                      <p className="lg:text-[1vw] sm:text-[1.1vh] text-[1.2vh] text-neutral-700 font-medium">{comment.content}</p>
 
                     </div>
                   ) )}
@@ -176,8 +176,8 @@ export default function Post(){
 
                 <div className="flex justify-center items-center">
                   <div onClick={() => navigate("/feed")} className="flex px-[2vh] py-[1vh] bg-neutral-700 mb-[5vh] text-white rounded-2xl cursor-pointer hover:bg-neutral-800">
-                    <p className="text-[1.1vw]">Read more</p>
-                    <ChevronsRight style={{height: '2.7vh'}} />
+                    <p className="lg:text-[1.1vw] sm:text-[1.3vh] text-[1.4vh]">Read more</p>
+                    <ChevronsRight className="lg:h-[2.7vh] h-[2vh]" />
                   </div>
                 </div>
 
