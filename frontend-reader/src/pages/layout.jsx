748c1b98e6404f117a43651da92b2860e214b322
amriteshx1 <amriteshx18@gmail.com>
2025-06-10 11:40:50 +0530

    some final tweaks in main-nav responsiveness

--- frontend-reader/src/pages/layout.jsx
+++ frontend-reader/src/pages/layout.jsx
@@ -41,17 +41,17 @@ export default function Layout() {
   return (
     <div className="main-container flex-col lg:px-[10vh] sm:px-[2vh] px-[1vh] overflow-y-scroll">
       <div className="lg:h-[9vh] h-[8vh]  w-full flex justify-between items-center bg-neutral-700 text-zinc-100 p-[2vh] rounded-b-4xl">
-        <div className="w-[50%] flex justify-start items-center lg:gap-[3vw] gap-[3vh]">
+        <div className="sm:w-[50%] w-[70%] flex justify-start items-center lg:gap-[3vw] sm:gap-[3vh] gap-[1.5vh]">
           <p onClick={() => navigate("/feed")} className="lg:text-[1.7vw] text-[2.2vh] font-medium hover:cursor-pointer">Wordloom.</p>
           <input
             type="text"
-            className="lg:h-[4vh] h-[2.5vh] w-[50%] bg-white rounded-2xl text-neutral-700 lg:p-[1.5vh] p-[1vh] border-2 border-neutral-700 lg:text-[1vw] text-[1.1vh] font-normal"
+            className="lg:h-[4vh] h-[2.5vh] w-[50%] bg-white rounded-2xl text-neutral-700 lg:p-[1.5vh] p-[1vh] border-2 border-neutral-700 lg:text-[1vw] text-[1.1vh] font-normal focus:outline-none"
             placeholder="Search blogs...."
           />
         </div>
 
-        <div className="w-[50%] flex justify-end items-center lg:gap-[2vw] gap-[1.5vh]">
-          <div className="w-auto flex justify-center items-center lg:gap-1 gap-2">
+        <div className="sm:w-[50%] w-[30%] flex justify-end items-center lg:gap-[2vw] sm:gap-[1.5vh] gap-[0.2vh]">
+          <div className="hidden sm:flex w-auto justify-center items-center lg:gap-1 gap-2">
             <img src={write} alt="write-blog-logo" className="lg:h-[2.8vh] h-[1.9vh]" />
             <button
               onClick={handleWrite}
@@ -70,7 +70,7 @@ export default function Layout() {
             {open && (
               <div className="absolute lg:h-[12vh] h-[8vh] right-0 flex flex-col justify-center items-center lg:mt-[5vh] mt-[4vh] mr-[1.5vh] lg:w-[10vw] w-[12vh] bg-white border border-gray-300 rounded-tl-4xl  rounded-br-4xl shadow-lg z-50">
                 <button
-                  className=" h-[50%] w-full lg:px-6 lg:py-2 px-4 py-1 text-left text-gray-700 rounded-tl-4xl cursor-pointer hover:bg-gray-100"
+                  className=" h-[50%] w-full lg:px-6 lg:py-2 px-4 py-1 text-left lg:text-[1.1vw] text-[1.2vh] text-gray-700 rounded-tl-4xl cursor-pointer hover:bg-gray-100"
                   onClick={() => {
                     handleWrite();
                     setOpen(false);
@@ -79,7 +79,7 @@ export default function Layout() {
                   Admin Portal
                 </button>
                 <button
-                  className=" h-[50%] w-full lg:px-6 lg:py-2 px-4 py-1 text-left text-red-600  rounded-br-4xl cursor-pointer hover:bg-gray-100"
+                  className=" h-[50%] w-full lg:px-6 lg:py-2 px-4 py-1 text-left lg:text-[1.1vw] text-[1.2vh] text-red-600  rounded-br-4xl cursor-pointer hover:bg-gray-100"
                   onClick={handleLogout}
                 >
                   Sign Out
