c70d794b4957b1c2201ca05e59f92335eadc41ea
amriteshx1 <amriteshx18@gmail.com>
2025-06-11 20:12:13 +0530

    add responsiveness to comments-page

--- frontend-admin/src/pages/comments.jsx
+++ frontend-admin/src/pages/comments.jsx
@@ -63,32 +63,32 @@ function Comments(){
         <div className="min-h-full w-full flex flex-col justify-start items-center gap-[4vh] p-[2vh] bg-neutral-800">
 
             <div className="w-[100%] flex flex-col justify-center items-center p-[2vh]">
-            <p className="text-[2.3vw] text-white font-bold">Comments!</p>
-            <hr className="w-[30%] border-2 border-white" />
+            <p className="lg:text-[2.3vw] text-[2.3vh] text-white font-bold">Comments!</p>
+            <hr className="lg:w-[30%] w-[50%] border-2 border-white" />
             </div>
 
             {comments && comments.length > 0 ? (
-                comments.map((item, index) => <div key={item.id} className="p-[2vh] w-full flex justify-between items-center bg-neutral-900 rounded-xl text-white">
-                    <div className="h-full w-[75%] flex justify-between items-center">
+                comments.map((item, index) => <div key={item.id} className="p-[2vh] w-full flex lg:flex-row flex-col lg:justify-between justify-center lg:gap-0 gap-[2vh] items-center bg-neutral-900 rounded-xl text-white">
+                    <div className="h-full lg:w-[75%] w-full flex justify-between items-center">
                         <div className="h-full w-[65%] flex justify-start items-center whitespace-normal">
-                        <p className="text-[1.2vw]"><span className="font-bold">{item.author.username}: </span>{item.content}</p>
+                        <p className="lg:text-[1.2vw] text-[1.5vh]"><span className="font-bold">{item.author.username}: </span>{item.content}</p>
                         </div>
 
                         <div className="w-[25%] flex justify-start items-center">
-                        <p className="text-[1.2vw]"><span className="font-bold">Blog Id: </span>{item.postId}</p>
+                        <p className="lg:text-[1.2vw] text-[1.5vh]"><span className="font-bold">Blog Id: </span>{item.postId}</p>
                         </div>
 
                     </div>
                     
-                    <div className="h-full w-[20%] flex justify-between items-center">
-                        <button type="button" onClick={() => handleEdit(item.id, item.content)} className="px-[4vh] py-[0.5vh] bg-white text-neutral-900 rounded-xl font-bold cursor-pointer hover:bg-neutral-200">Edit</button>
-                        <button type="button" onClick={() => handleDelete(item.id)} className="px-[3vh] py-[0.5vh] bg-white text-neutral-900 rounded-xl font-bold cursor-pointer hover:bg-neutral-200">Delete</button>
+                    <div className="h-full lg:w-[20%] w-full flex lg:justify-between justify-center lg:gap-0 gap-[2vh] items-center">
+                        <button type="button" onClick={() => handleEdit(item.id, item.content)} className="lg:px-[4vh] lg:py-[0.5vh] px-[2vh] py-[0.3vh] bg-white lg:text-[1vw] text-[1.4vh] text-neutral-900 rounded-xl font-bold cursor-pointer hover:bg-neutral-200">Edit</button>
+                        <button type="button" onClick={() => handleDelete(item.id)} className="lg:px-[3vh] lg:py-[0.5vh] px-[1.8vh] py-[0.3vh] bg-white lg:text-[1vw] text-[1.4vh] text-neutral-900 rounded-xl font-bold cursor-pointer hover:bg-neutral-200">Delete</button>
                     </div>
                     
                     
                     </div>)
             ) : (
-                <p className="text-[1.2vw] text-white font-medium">You don't have any comments yet.</p>
+                <p className="lg:text-[1.2vw] text-[1.3vh] text-white font-medium">You don't have any comments yet.</p>
             )}
 
 
