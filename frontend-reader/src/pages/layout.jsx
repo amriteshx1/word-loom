d77969f8028252eedb5b9ee14b77e79a444e799c
amriteshx1 <amriteshx18@gmail.com>
2025-06-15 23:41:10 +0530

    updated admin switch link

--- frontend-reader/src/pages/layout.jsx
+++ frontend-reader/src/pages/layout.jsx
@@ -27,7 +27,7 @@ export default function Layout() {
     );
 
     if (adminAlert) {
-      window.open("word-loom-mocha.vercel.app", "_blank");
+      window.open("https://word-loom-mocha.vercel.app", "_blank");
     }
   };
 
